4440c0321b841c7b252e5b06423bdf2756902f18
chencody <chencody@qq.com>
2021-11-10 20:45:11 +0800

    update custom err info Tag:errMsg

--- global/validator.go
+++ global/validator.go
@@ -42,14 +42,15 @@ func (v *Validate) FormatError(u interface{}, err error) error {
 		for _, err := range err.(validator.ValidationErrors) {
 			// 判断是否有自定义错误信息
 			fieldName := err.Field()
+			customerErrInfo := ""
 			field, ok := reflect.TypeOf(u).FieldByName(fieldName)
 			if ok {
-				customerErrInfo := field.Tag.Get("errMsg")
-				if customerErrInfo != "" {
-					errStr = append(errStr, customerErrInfo)
-				} else {
-					errStr = append(errStr, err.Translate(*v.Trans))
-				}
+				customerErrInfo = field.Tag.Get("errMsg")
+			}
+			if customerErrInfo != "" {
+				errStr = append(errStr, customerErrInfo)
+			} else {
+				errStr = append(errStr, err.Translate(*v.Trans))
 			}
 		}
 		return errors.New(strings.Join(errStr, "|"))
