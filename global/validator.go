76fb8051e2a57b3e55f505b9dae649249d9a8fbd
chencody <chencody@qq.com>
2021-11-10 20:27:20 +0800

    test custom err info Tag

--- global/validator.go
+++ global/validator.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	"reflect"
 	"strings"
 )
 
@@ -19,11 +20,20 @@ type Validate struct {
 //  @param err
 //  @return error
 //
-func (v *Validate) FormatError(err error) error {
+func (v *Validate) FormatError(u interface{}, err error) error {
 	if _, ok := err.(validator.ValidationErrors); ok {
 		// 验证反馈错误
 		var errStr []string
 		for _, err := range err.(validator.ValidationErrors) {
+			// 判断是否有自定义错误信息
+			fieldName := err.Field()
+			field, ok := reflect.TypeOf(u).FieldByName(fieldName)
+			if ok {
+				customerErrInfo := field.Tag.Get("errMsg")
+				if customerErrInfo != "" {
+					errStr = append(errStr, customerErrInfo)
+				}
+			}
 			errStr = append(errStr, err.Translate(*v.Trans))
 		}
 		return errors.New(strings.Join(errStr, "|"))
