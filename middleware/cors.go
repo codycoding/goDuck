8df1a449298e30c3f81817c0c6afb91a941a8499
chencody <chencody@qq.com>
2021-11-14 13:35:59 +0800

    update cors 中间件

--- middleware/cors.go
+++ middleware/cors.go
@@ -15,11 +15,13 @@ func Cors() gin.HandlerFunc {
 	switch global.Config.Cors.CorsMode {
 	case "none":
 		// 关闭跨域
+		global.Log.Info("cors中间已关闭")
 		return func(c *gin.Context) {
 			c.Next()
 		}
 	case "config":
 		// 按配置处理跨域
+		global.Log.Info("cors中间按自定义配置启动")
 		return cors.New(cors.Config{
 			AllowAllOrigins:        false,
 			AllowOrigins:           global.Config.Cors.AllowOrigins,
@@ -36,6 +38,7 @@ func Cors() gin.HandlerFunc {
 		})
 	default:
 		// 默认跨域设置
+		global.Log.Info("cors中间默认配置启动")
 		return cors.Default()
 	}
 }
