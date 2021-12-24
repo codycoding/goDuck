c31788d3959344a43f0b60fac943208f91625aa8
chencody <chencody@qq.com>
2021-12-24 16:08:51 +0800

    update casbin 复合角色判断

--- middleware/casbin.go
+++ middleware/casbin.go
@@ -7,6 +7,16 @@ import (
 	"net/http"
 )
 
+// UserToRoles 临时角色-用户对应表
+type UserToRoles struct {
+	UserId int64  `json:"userId" gorm:"account_myuser_id"`
+	RoleId string `json:"roleId" gorm:"authority_role_authority_id"`
+}
+
+func (UserToRoles) TableName() string {
+	return "authority_user_role"
+}
+
 //
 // CasbinHandler
 //  @Description: API访问权限Casbin鉴权
@@ -22,10 +32,25 @@ func CasbinHandler() gin.HandlerFunc {
 		obj := c.Request.URL.Path
 		// 获取请求方法
 		act := c.Request.Method
-		// 获取用户的角色
-		sub := waitUse.UserInfo.RoleId
-		// 判断策略中是否存在
-		success, _ := global.Casbin.Enforce(sub, obj, act)
+		// 获取用户的所有角色
+		userId := waitUse.UserInfo.AccountId
+		var roleIds []string
+		db := global.PostgresDb.Model(&UserToRoles{}).Select("authority_role_authority_id").Where("account_myuser_id = ?", userId)
+		if err := db.Scan(&roleIds).Error; err != nil {
+			core.FailWithMessage(http.StatusBadRequest, "权限角色查询失败", c)
+			c.Abort()
+			return
+		}
+		success := false
+		// 循环角色判断策略中是否存在
+		for _, roleId := range roleIds {
+			// 获取用户的角色
+			sub := roleId
+			success, _ = global.Casbin.Enforce(sub, obj, act)
+			if success {
+				break
+			}
+		}
 		// 开发环境跳过访问限制
 		if global.Config.System.Env == "develop" || success {
 			c.Next()
